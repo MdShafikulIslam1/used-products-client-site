2b2a949836e8179d6552c4732f59e91627e6ec5e
MdShafikulIslam <179398ssshofik@gmail.com>
2022-11-27 11:45:21 +0600

    meaningful notfound page is setup

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import notFoundImg from '../../images/login/n.png'
 
 const NotFoundPage = () => {
     return (
-        <div className='my-12'>
-            <h1>This is NotFound Page (404)</h1>
-            <figure>
-                <img className='w-4/5 rounded-xl' src={notFoundImg} alt="" />
+        <div className='my-3'>
+            <h1 className='text-2xl font-bold text-center text-red-900 p-5'>This is NotFound Page (404)</h1>
+            <figure className='w-[600px]  mx-auto'>
+                <div>
+                    <img className='rounded-xl' src={notFoundImg} alt="" />
+                </div>
+                <Link to='/'><button className='btn btn-primary w-full mt-5'>Go to Homepage.</button></Link>
             </figure>
         </div>
     );
